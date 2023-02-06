3caa455016f2fff5b7698953875b863cbbee3d39
eiei114 <nowaka37564@gmail.com>
2023-02-06 13:52:42 +0900

    fix eslintエラー修正、インポートの順番変更＃5

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,32 +1,32 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import Home from "../pages/index";
+import Home from '../pages/index'
 
 describe('Home', () => {
-    it('renders a list of blog posts', () => {
-        const mockPosts = [
-            {
-                slug: 'sample-post-1',
-                frontMatter: {
-                    title: 'Sample Post 1',
-                    description: 'This is a sample post 1'
-                }
-            },
-            {
-                slug: 'sample-post-2',
-                frontMatter: {
-                    title: 'Sample Post 2',
-                    description: 'This is a sample post 2'
-                }
-            }
-        ]
-        render(<Home posts={mockPosts} />)
+  it('renders a list of blog posts', () => {
+    const mockPosts = [
+      {
+        slug: 'sample-post-1',
+        frontMatter: {
+          title: 'Sample Post 1',
+          description: 'This is a sample post 1',
+        },
+      },
+      {
+        slug: 'sample-post-2',
+        frontMatter: {
+          title: 'Sample Post 2',
+          description: 'This is a sample post 2',
+        },
+      },
+    ]
+    render(<Home posts={mockPosts} />)
 
-        mockPosts.forEach(({ slug, frontMatter: { title, description } }) => {
-            const titleElement = screen.getByText(title)
-            expect(titleElement).toBeInTheDocument()
-            const descriptionElement = screen.getByText(description)
-            expect(descriptionElement).toBeInTheDocument()
-        })
+    mockPosts.forEach(({ slug, frontMatter: { title, description } }) => {
+      const titleElement = screen.getByText(title)
+      expect(titleElement).toBeInTheDocument()
+      const descriptionElement = screen.getByText(description)
+      expect(descriptionElement).toBeInTheDocument()
     })
+  })
 })
