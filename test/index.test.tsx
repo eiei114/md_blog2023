75aa019703ad2f94dec8c45ec9ceb50680c189e0
eiei114 <nowaka37564@gmail.com>
2023-02-11 10:44:42 +0900

    fix test修正

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,32 +1,39 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import Home from '../pages/index'
+import Home from './Home'
+
+const samplePosts = [
+  {
+    slug: 'post-1',
+    frontMatter: {
+      title: 'Post 1',
+      description: 'This is the first post',
+      date: '2021-01-01'
+    }
+  },
+  {
+    slug: 'post-2',
+    frontMatter: {
+      title: 'Post 2',
+      description: 'This is the second post',
+      date: '2021-02-01'
+    }
+  }
+]
 
 describe('Home', () => {
-  it('renders a list of blog posts', () => {
-    const mockPosts = [
-      {
-        slug: 'sample-post-1',
-        frontMatter: {
-          title: 'Sample Post 1',
-          description: 'This is a sample post 1',
-        },
-      },
-      {
-        slug: 'sample-post-2',
-        frontMatter: {
-          title: 'Sample Post 2',
-          description: 'This is a sample post 2',
-        },
-      },
-    ]
-    render(<Home posts={mockPosts} />)
+  it('renders the correct number of posts', () => {
+    render(<Home posts={samplePosts} />)
 
-    mockPosts.forEach(({ slug, frontMatter: { title, description } }) => {
-      const titleElement = screen.getByText(title)
-      expect(titleElement).toBeInTheDocument()
-      const descriptionElement = screen.getByText(description)
-      expect(descriptionElement).toBeInTheDocument()
-    })
+    const postTitles = screen.getAllByRole('heading', { level: 5 })
+    expect(postTitles).toHaveLength(2)
+  })
+
+  it('renders the correct post titles', () => {
+    render(<Home posts={samplePosts} />)
+
+    const postTitles = screen.getAllByRole('heading', { level: 5 })
+    expect(postTitles[0]).toHaveTextContent('Post 2')
+    expect(postTitles[1]).toHaveTextContent('Post 1')
   })
 })
