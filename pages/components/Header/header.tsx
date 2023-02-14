141a7b043941077c0ae3fd242d6f923941ed8d5d
eiei114 <nowaka37564@gmail.com>
2023-02-14 16:44:14 +0900

    css修正

--- pages/components/Header/header.tsx
+++ pages/components/Header/header.tsx
@@ -18,7 +18,7 @@ const Header: FC = () => {
     return (
         <header className="bg-gray-900 text-white py-6">
             <h1 className="text-2xl font-medium mx-auto max-w-sm ">Poetic Coding</h1>
-            <nav className="flex justify-center mt-4">
+            <nav className="flex justify-center flex-wrap mt-4">
                 <Link href="/" className="px-4 text-white hover:bg-gray-800">
                     Home
                 </Link>
@@ -28,26 +28,31 @@ const Header: FC = () => {
                 <Link href="/disclaimer" className="px-4 text-white hover:bg-gray-800 ml-4">
                     Disclaimer
                 </Link>
-                {!currentUser ? (
-                    <>
-                        <Link href="/signin" className="px-4 text-white hover:bg-gray-800 ml-auto mr-4">
-                            SignIn
-                        </Link>
-                        <Link href="/signup" className="px-4 text-white hover:bg-gray-800">
-                            SignUp
-                        </Link>
-                    </>
-                ) : (
-                    <button onClick={handleSignOut} className="px-4 text-white hover:bg-gray-800 ml-auto mr-4">
-                        Sign Out
-                    </button>
+                {!currentUser && (
+                    <div className="text-center py-2 text-red-500 w-full">
+                        ログインするとコメントが投稿できます！
+                    </div>
                 )}
-            </nav>
-            {!currentUser && (
-                <div className="text-center py-2 text-red-500">
-                    ログインするとコメントが投稿できます！
+                <div className="ml-auto">
+                    {!currentUser ? (
+                        <>
+                            <Link href="/signin" className="px-4 text-white hover:bg-gray-800">
+                                SignIn
+                            </Link>
+                            <Link href="/signup" className="px-4 text-white hover:bg-gray-800 ml-4">
+                                SignUp
+                            </Link>
+                        </>
+                    ) : (
+                        <button
+                            onClick={handleSignOut}
+                            className="px-4 text-white hover:bg-gray-800"
+                        >
+                            Sign Out
+                        </button>
+                    )}
                 </div>
-            )}
+            </nav>
         </header>
     );
 };
